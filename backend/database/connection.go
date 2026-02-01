5418f9bf433dac49d9f233d59b90849596dcd16a
ahmed salebaan <ahmedsalebaancrtan@gmail.com>
2026-02-01 16:36:25 +0300

    fix: support DATABASE_URL env var for cloud deployment

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -13,14 +13,21 @@ var DB *sql.DB
 
 // Connect establishes a connection to the PostgreSQL database
 func Connect() error {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	var connStr string
+	dbURL := os.Getenv("DATABASE_URL")
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	if dbURL != "" {
+		connStr = dbURL
+	} else {
+		host := os.Getenv("DB_HOST")
+		port := os.Getenv("DB_PORT")
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		dbname := os.Getenv("DB_NAME")
+
+		connStr = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbname)
+	}
 
 	var err error
 	DB, err = sql.Open("postgres", connStr)
